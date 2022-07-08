fafe0d8db97df19a75e2429e32149682bd14b2ee
xx <xx@xx.xx>
2022-07-08 22:31:55 +0800

    Add: arm64

--- build/docker/build.sh
+++ build/docker/build.sh
@@ -14,10 +14,16 @@ if [ -f ./target/x86_64-unknown-linux-musl/release/vocechat-server ]; then
   cd build/docker
 
   # sudo docker login -u vocechat -p xxxxxx
-  sudo docker build -t vocechat-server:latest .
+  # build arm64
+  sudo docker build --platform=linux/amd64 -t vocechat-server:latest .
   docker tag vocechat-server:latest Privoce/vocechat-server:latest
   docker push Privoce/vocechat-server:latest
 
+  # build arrch64
+  cp -rf ./target/aarch64-unknown-linux-musl/release/vocechat-server build/docker/vocechat-server
+  sudo docker build --platform=linux/arm64 -t vocechat-server:latest-arm64 .
+  docker tag vocechat-server:latest-arm64 Privoce/vocechat-server:latest-arm64
+  docker push Privoce/vocechat-server:latest-arm64
   #rm -rf  ./config
 
 fi
