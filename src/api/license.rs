5405fc219993d8361287a4d5c3b7e23e4d633aa9
xx <xx@xx.xx>
2022-07-11 23:30:54 +0800

    license get

--- src/api/license.rs
+++ src/api/license.rs
@@ -20,6 +20,7 @@ struct SaveLicenseRequest {
 #[derive(Debug, Object, Clone)]
 pub struct LicenseReply {
     pub domain: String,
+    pub user_limit: u32,
     pub created_at: chrono::DateTime<Utc>,
     pub expired_at: chrono::DateTime<Utc>,
     pub sign: bool,
@@ -53,6 +54,7 @@ impl ApiLicense {
 
         Ok(Json(LicenseReply {
             domain: license.domain.clone(),
+            user_limit: license.user_limit,
             created_at: license.created_at,
             expired_at: license.expired_at,
             sign: sign_is_ok,
@@ -60,8 +62,8 @@ impl ApiLicense {
     }
 
     /// Save the license
-    #[oai(path = "/save", method = "post")]
-    async fn save(
+    #[oai(path = "/", method = "put")]
+    async fn put(
         &self,
         state: Data<&State>,
         token: Token,
@@ -76,6 +78,33 @@ impl ApiLicense {
                 .map_err(|_err| Error::from_status(StatusCode::BAD_REQUEST))?;
         }
         Ok(Json(true))
+    }
+
+    /// Get the license
+    #[oai(path = "/", method = "get")]
+    async fn get(
+        &self,
+        state: Data<&State>,
+        //token: Token,
+    ) -> Result<Json<LicenseReply>> {
+        // if !token.is_admin {
+        //     // return Err(Error::from_status(StatusCode::FORBIDDEN));
+        // }
+        let mut license_path = state.config.system.data_dir.clone();
+        license_path.push("license");
+        let license_bs58 = tokio::fs::read_to_string(license_path).await.map_err(|_err|Error::from_status(StatusCode::BAD_REQUEST))?;
+        let license = vc_license::License::from_string(license_bs58.clone())
+            .map_err(|_err| Error::from_status(StatusCode::BAD_REQUEST))?;
+        let sign_is_ok =
+            vc_license::rsa_check_license_bs58(&license_bs58, VOCE_LICENSE_PUBLIC_KEY_PEM).is_ok();
+
+        Ok(Json(LicenseReply {
+            domain: license.domain.clone(),
+            user_limit: license.user_limit,
+            created_at: license.created_at,
+            expired_at: license.expired_at,
+            sign: sign_is_ok,
+        }))
     }
 }
 
@@ -118,7 +147,7 @@ mod tests {
     async fn test_save_license() {
         let server = TestServer::new().await;
         let resp = server
-            .post("/api/license/save")
+            .put("/api/license")
             .body_json(&json!({
                         "license": "2RvN4krfyQbvuLcXzz4PwoJfckRFptqLKKXSP7f4Np7AGqgixrfdUzZ5qk2iT2gFwCntivyYnSaumyao7QF2SfQ3TKyLAgGDYeYeJcbBri9re5esG2PMrxZBjq68eR94yYqdTg6LAP9oc5WtLAaBX25RkVu2zt59kdgRzk5CbnnmZnMAHgZEDnsVJfCQa6HKnb3p1cpZa6LTANrKh1VuvCAerdCHCoc1YHNwUipg5JJXmxJQadFShv1sYREUHHzLuPMb8xHb7GkPJ6MJpQBzHfDnTySRG7BGxNz5GKCutFp1o7YjkQqZvKBTvWQ8ic4HmbHErLHw4aJ5CAMyugX4v2GAgxS8Z4zi9tjdzRtbbqzncnXSNBTumBxobBskNAbEaQC9HgBavgcAw4wHrHSyG3v2rTdsDUJZgTtanEfxnxZhSpKtXZRFzVNdjmo66GeLhvWwMZSXKzH89uTvMcEokmbUyLz9mKXzRP9dhTJ4bC6YWNrbDueYX9pqrsmXm3Z4aYP7DjknXwPCMKZbsCXZi2YQVUjggCyRarR4eThY6gZ8iWGkvi1ybAADooh8KXSAFNGSRRTGC5La6Atug7y6e6QnFmaRndLHdCyxqyq9LM1Ly2icYAFa2ZspXyL3MyBTLFvgQeqTmL1KQVHzhjwtkTvcFsUxScYNXhVgCkyD5vkSZpcwixGJYyYtkrF27XpMcq8mR6dAyi3Zqt2w68X3xA748a8ofky1KYwHmA1U4BaDpXbbVKSu6wtonMhzvQ6xMssyWJVhrzeymPnMwM8Xiut3pZcpC77Ri",
                     }))
