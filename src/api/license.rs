47047fe98d25a80ea23e0727d6d5e03fcdb0f861
Sunli <scott_s829@163.com>
2022-09-28 09:59:41 +0800

    Update license.rs

--- src/api/license.rs
+++ src/api/license.rs
@@ -85,9 +85,6 @@ impl ApiLicense {
     /// Get the license
     #[oai(path = "/", method = "get")]
     async fn get(&self, state: Data<&State>, token: Token) -> Result<Json<LicenseReply>> {
-        if !token.is_admin {
-            return Err(Error::from_status(StatusCode::FORBIDDEN));
-        }
         let mut license_path = state.config.system.data_dir.clone();
         license_path.push("license");
         let license_bs58 = tokio::fs::read_to_string(license_path)
