cbf04f4f98338552eec184584656d49ee1025066
Sunli <scott_s829@163.com>
2022-11-30 12:49:56 +0800

    Update admin_fcm.rs

--- src/api/admin_fcm.rs
+++ src/api/admin_fcm.rs
@@ -14,6 +14,7 @@ pub struct ApiAdminFirebase;
 /// Firebase config
 #[derive(Debug, Object, Serialize, Deserialize, Default)]
 pub struct FcmConfig {
+    #[serde(default)]
     use_offical: bool,
     #[oai(default = "default_token_url")]
     token_url: String,
